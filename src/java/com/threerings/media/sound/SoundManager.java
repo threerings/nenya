fbf539580e04a0337894aa9601b8e05a1a98ebc8
Charlie Groves <charlie@threerings.net>
2008-08-14 00:51:34 +0000

    Readd a deprecated SoundManager that just extends JavaSoundPlayer to keep it around with its original name
    git-svn-id: svn+ssh://src.earth.threerings.net/nenya/trunk@626 ed5b42cb-e716-0410-a449-f6a68f950b19

---
+++ src/java/com/threerings/media/sound/SoundManager.java
@@ -0,0 +1,21 @@
+package com.threerings.media.sound;
+
+import com.threerings.resource.ResourceManager;
+
+/**
+ * Exposes JavaSoundPlayer under its old name.
+ *
+ * @deprecated - use {@link JavaSoundPlayer}
+ */
+public class SoundManager extends JavaSoundPlayer
+{
+    public SoundManager (ResourceManager rmgr)
+    {
+        super(rmgr);
+    }
+
+    public SoundManager (ResourceManager rmgr, String defaultBundle, String defaultClip)
+    {
+        super(rmgr);
+    }
+}
